a6a6cb62e8cf6ce7987c821f5547329803c1a60d
karthikeya-vadlamani <karthikeyavadlamani@gmail.com>
2026-04-14 00:14:43 +0530

    testing CD

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,7 +18,7 @@ app.get("/health", async (req, res) => {
   try {
     await prisma.$queryRaw`SELECT 1`;
     res.status(200).json({
-      status: "UP",
+      status: "UP connected",
       database: "CONNECTED",
       service: "backend-api",
     });

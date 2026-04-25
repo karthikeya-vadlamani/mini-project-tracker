b0256602ff218a922e1ff3129aadea1d6606b65e
karthikeya-vadlamani <karthikeyavadlamani@gmail.com>
2026-04-25 16:49:21 +0530

    fixing backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,14 +2,13 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const { PrismaClient } = require("@prisma/client");
+const client = require('prom-client');
 
 dotenv.config();
 
 const app = express();
 const prisma = new PrismaClient();
 const router = express.Router(); // Create the router
-
-import client from 'prom-client';
 
 const collectDefaultMetrics = client.collectDefaultMetrics;
 collectDefaultMetrics();
